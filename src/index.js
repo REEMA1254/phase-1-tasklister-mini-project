f4c76254ce62246590cfd8602014a81ab04c3c2c
maryam <Reemaali9048@gmail.com>
2023-10-17 12:34:14 +0300

    complete assigment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();  
+
+    const taskDescription = taskInput.value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function() {
+      taskList.removeChild(taskItem);
+    })
+
+  
+    taskList.appendChild(taskItem);
+
+    
+    taskInput.value = "";
+  });
 });
